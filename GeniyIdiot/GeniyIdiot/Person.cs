73112b5b0ac173a8576fe73c9adaee3a2671da4b
Kristina Siunik <anitanit43@gmail.com>
2020-05-23 20:53:36 +0300

    создала новую ветку

---
+++ GeniyIdiot/GeniyIdiot/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeniyIdiot
+{
+    class Person
+    {
+        public string Name;
+        public string Diagnose;
+        public int RightAnswers;
+    }
+}
---
+++ GeniyIdiot/GeniyIdiot/Question.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeniyIdiot
+{
+    class Question
+    {
+        public string Text;
+        public int Answer;
+        public Question(string text, int answer)
+        {
+            Text = text;
+            Answer = answer;
+        }
+        public string Print()
+        {
+            return "Вопрос" + Text;
+        }
+    }
+}

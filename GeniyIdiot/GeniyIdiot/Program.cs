e97e0d1cc0ae40697388b302b7ddff3e53d47c23
Kristina Siunik <anitanit43@gmail.com>
2020-05-28 19:41:18 +0300

    исправила сохранение данных в файл

--- GeniyIdiot/GeniyIdiot/Program.cs
+++ GeniyIdiot/GeniyIdiot/Program.cs
@@ -67,7 +67,7 @@ namespace GeniyIdiot
         static void SaveResults(string path, User user)
         {
             var results = new StreamWriter(path, true);
-            results.WriteLine(user.Name + " "+ user.Surname + 
+            results.WriteLine(user.Name + " "+ user.Surname + " " +
                 user.CountRightAnswers.ToString() + " " + user.Diagnose);
             results.Close();
         }
@@ -79,12 +79,12 @@ namespace GeniyIdiot
                     using (StreamReader streamReader = new StreamReader(path, true))
                     {
                        var line = streamReader.ReadToEnd().Split('\n');
-                        Console.WriteLine("{0,-25} {0,-25} {1,-30} {2, 20}\n", 
+                        Console.WriteLine("{0,-25} {1,-25} {2,-35} {3, 20}\n", 
                             "Имя:", "Фамилия:","Количество правильных ответов:", "Диагноз:");
                         for (int i = 0; i < line.Length - 1; i++)
                         {
                             var world = line[i].Split(' ');
-                            Console.WriteLine("{0,-25} {0,-25} {1,-30} {2, 20}\n", world[0], world[1], world[2], world[3]);
+                            Console.WriteLine("{0,-25} {1,-25} {2,-35} {3, 20}\n", world[0], world[1], world[2], world[3]);
                         }
                     }
                 }

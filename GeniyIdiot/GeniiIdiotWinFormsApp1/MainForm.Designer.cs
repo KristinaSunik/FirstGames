d1cfe8dad817db374efc3c1b55306cb2e02d8950
Kristina Siunik <anitanit43@gmail.com>
2020-06-09 15:10:23 +0300

    сделала меню

--- GeniyIdiot/GeniiIdiotWinFormsApp1/MainForm.Designer.cs
+++ GeniyIdiot/GeniiIdiotWinFormsApp1/MainForm.Designer.cs
@@ -32,13 +32,20 @@
             this.questionNumberLabel = new System.Windows.Forms.Label();
             this.nextQuestionButton = new System.Windows.Forms.Button();
             this.userAnswerTextBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.посмотретьПредыдущиеРезультатыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.рестартToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2.SuspendLayout();
             this.SuspendLayout();
             // 
             // questionTextLabel
             // 
             this.questionTextLabel.AutoSize = true;
             this.questionTextLabel.Font = new System.Drawing.Font("Microsoft YaHei", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.questionTextLabel.Location = new System.Drawing.Point(41, 83);
+            this.questionTextLabel.Location = new System.Drawing.Point(89, 117);
             this.questionTextLabel.Name = "questionTextLabel";
             this.questionTextLabel.Size = new System.Drawing.Size(153, 27);
             this.questionTextLabel.TabIndex = 0;
@@ -48,7 +55,7 @@
             // 
             this.questionNumberLabel.AutoSize = true;
             this.questionNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.questionNumberLabel.Location = new System.Drawing.Point(40, 24);
+            this.questionNumberLabel.Location = new System.Drawing.Point(40, 44);
             this.questionNumberLabel.Name = "questionNumberLabel";
             this.questionNumberLabel.Size = new System.Drawing.Size(164, 32);
             this.questionNumberLabel.TabIndex = 1;
@@ -57,7 +64,7 @@
             // 
             // nextQuestionButton
             // 
-            this.nextQuestionButton.Location = new System.Drawing.Point(405, 114);
+            this.nextQuestionButton.Location = new System.Drawing.Point(400, 183);
             this.nextQuestionButton.Name = "nextQuestionButton";
             this.nextQuestionButton.Size = new System.Drawing.Size(181, 83);
             this.nextQuestionButton.TabIndex = 2;
@@ -67,21 +74,76 @@
             // 
             // userAnswerTextBox
             // 
-            this.userAnswerTextBox.Location = new System.Drawing.Point(46, 142);
+            this.userAnswerTextBox.Location = new System.Drawing.Point(46, 211);
             this.userAnswerTextBox.Name = "userAnswerTextBox";
             this.userAnswerTextBox.Size = new System.Drawing.Size(327, 26);
             this.userAnswerTextBox.TabIndex = 3;
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 33);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(610, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(610, 33);
+            this.menuStrip2.TabIndex = 5;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.посмотретьПредыдущиеРезультатыToolStripMenuItem,
+            this.рестартToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(69, 29);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // посмотретьПредыдущиеРезультатыToolStripMenuItem
+            // 
+            this.посмотретьПредыдущиеРезультатыToolStripMenuItem.Name = "посмотретьПредыдущиеРезультатыToolStripMenuItem";
+            this.посмотретьПредыдущиеРезультатыToolStripMenuItem.Size = new System.Drawing.Size(420, 34);
+            this.посмотретьПредыдущиеРезультатыToolStripMenuItem.Text = "Посмотреть предыдущие результаты";
+            // 
+            // рестартToolStripMenuItem
+            // 
+            this.рестартToolStripMenuItem.Name = "рестартToolStripMenuItem";
+            this.рестартToolStripMenuItem.Size = new System.Drawing.Size(420, 34);
+            this.рестартToolStripMenuItem.Text = "Рестарт";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(420, 34);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
             // GeniiIdiotWinFormsApp
             // 
-            this.ClientSize = new System.Drawing.Size(610, 224);
+            this.ClientSize = new System.Drawing.Size(610, 294);
             this.Controls.Add(this.userAnswerTextBox);
             this.Controls.Add(this.nextQuestionButton);
             this.Controls.Add(this.questionNumberLabel);
             this.Controls.Add(this.questionTextLabel);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "GeniiIdiotWinFormsApp";
             this.Text = "тест Гений-Идиот";
             this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,6 +155,12 @@
         private System.Windows.Forms.Label questionNumberLabel;
         private System.Windows.Forms.Button nextQuestionButton;
         private System.Windows.Forms.TextBox userAnswerTextBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem посмотретьПредыдущиеРезультатыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem рестартToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
     }
 }
 

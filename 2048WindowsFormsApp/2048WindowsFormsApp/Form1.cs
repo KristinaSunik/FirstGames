3f9943237f510d09a032a02969e920f98ce86255
Kristina Siunik <anitanit43@gmail.com>
2020-06-22 20:33:43 +0300

    пустое поле игры

--- 2048WindowsFormsApp/2048WindowsFormsApp/Form1.cs
+++ 2048WindowsFormsApp/2048WindowsFormsApp/Form1.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace _2048WindowsFormsApp
 {
     public partial class Form1 : Form
     {
+        private const int margin = 6;
+        private int mapSize = 4;
+        private Label[,] map;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +17,36 @@ namespace _2048WindowsFormsApp
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            InitMap();
+        }
 
+        private void InitMap()
+        {
+            map = new Label[mapSize, mapSize];
+
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    var newLabel = CreateLabel(i, j);
+                    Controls.Add(newLabel);
+                    map[i, j] = newLabel;
+                }
+            }
+        }
+
+        private Label CreateLabel(int rowIndex, int columnIndex)
+        {
+            var label = new Label();
+            label.BackColor = SystemColors.ActiveCaption;
+            label.Font = new Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, (byte)(204));
+            label.Size = new Size(70, 70);
+            label.TextAlign = ContentAlignment.MiddleCenter;
+
+            var x = 10 + columnIndex * (70 + margin);
+            var y = 70 + rowIndex * (70 + margin);
+            label.Location = new Point(x, y);
+            return label;
         }
     }
 }

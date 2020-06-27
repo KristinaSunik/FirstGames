fc7e16da1280c90ee06e879e660113fc385b9688
Kristina Siunik <anitanit43@gmail.com>
2020-06-27 20:05:42 +0300

    кнопка рисовать круг

---
+++ BallsWindowsFormsApp/BallsWindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace BallsWindowsFormsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.printButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // printButton
+            // 
+            this.printButton.Location = new System.Drawing.Point(672, 51);
+            this.printButton.Name = "printButton";
+            this.printButton.Size = new System.Drawing.Size(101, 38);
+            this.printButton.TabIndex = 0;
+            this.printButton.Text = "рисовать";
+            this.printButton.UseVisualStyleBackColor = true;
+            this.printButton.Click += new System.EventHandler(this.printButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.printButton);
+            this.Name = "MainForm";
+            this.Text = "Balls";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button printButton;
+    }
+}
+
